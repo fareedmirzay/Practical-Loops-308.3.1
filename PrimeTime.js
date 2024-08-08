8151edf1d85c755f121e732f72db51b71ff29981
fareedmirzay <fareedmirzay@ymail.com>
2024-08-08 15:13:02 -0400

    Prime number is completed

--- PrimeTime.js
+++ PrimeTime.js
@@ -6,3 +6,26 @@
 // As soon as you find the prime number, log that number and exit the loop
 // Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
 // Be careful! If you set n to a number too large, your loop could take a long time to process.
+
+
+let n = 4; 
+
+let currentNumber = n + 1;
+
+while (true) {
+  let isPrime = true;
+
+  for (let i = 2; i < currentNumber; i++) {
+    if (currentNumber % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    console.log(currentNumber);
+    break;
+  }
+
+  currentNumber++;
+}
