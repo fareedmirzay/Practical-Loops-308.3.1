0949fd355a1f0ee862db70e2683abc49134d0353
fareedmirzay <fareedmirzay@ymail.com>
2024-08-08 15:43:13 -0400

    Feeling Loopy is completed

--- FeelingLoopy.js
+++ FeelingLoopy.js
@@ -6,3 +6,27 @@
 // Log each row of data.
 // You do not need to format the data, the following works well.
 // console.log(cell1, cell2, cell3, cell4);
+
+
+
+
+let csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+let rows = csvData.split("\n"); 
+
+for (let row of rows) {
+  let cells = row.split(",");
+  console.log(cells[0], cells[1], cells[2], cells[3]); 
+}
+
+let csvData2 = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+
+let rows2 = csvData2.split("\n");
+
+for (let row of rows2) {
+  let cells = row.split(",");
+  console.log(cells[0], cells[1], cells[2], cells[3]);
+}
+
+
+
