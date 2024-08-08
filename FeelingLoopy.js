4d67f1472323e1780817dec7b453828c0d1652e2
fareedmirzay <fareedmirzay@ymail.com>
2024-08-08 14:24:22 -0400

    test commit 1

---
+++ FeelingLoopy.js
@@ -0,0 +1,8 @@
+// Your task is to write a script that accomplishes the following:
+// Loop through the characters of a given CSV string.
+// Store each “cell” of data in a variable.
+// When you encounter a comma, move to the next cell.
+// When you encounter the “\r\n” sequence, move to the next “row.”
+// Log each row of data.
+// You do not need to format the data, the following works well.
+// console.log(cell1, cell2, cell3, cell4);
